22fd62f54f64245ad8f1794e3b729a5ab0415172
janoujan <janoujan@laposte.net>
2023-03-23 01:55:37 +0100

    add cors

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const productRoutes = require('./routes/product');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors())
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
